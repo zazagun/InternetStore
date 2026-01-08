b3f4a96c88f48ab4aeb278df48fef9b5e623aaa5
Home <qwe>
2026-01-08 19:56:29 +0400

    basket begins

--- client/src/pages/Basket.js
+++ client/src/pages/Basket.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Context } from "../index";
+import { useContext } from "react";
+import { Container } from "react-bootstrap";
 
 const Basket = () =>{
+    const { device } = useContext(Context)
+
     return (
-        <div>
-            <p>Basket</p>
-        </div>
+        <Container>
+            <p>add basket in the future</p>
+        </Container>
     )
 }
 
