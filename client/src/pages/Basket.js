cf079c7c8de15841ce267ba696c4284a0ac98d02
laptop <penis@gmail.com>
2026-01-12 12:25:11 +0400

    qwe

--- client/src/pages/Basket.js
+++ client/src/pages/Basket.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Context } from "../index";
 import { useContext } from "react";
 import { Container } from "react-bootstrap";
+import { addDevice, fetchFromBasket, deleteDevice } from "../http/deviceAPI"
 
 const Basket = () =>{
     const { device } = useContext(Context)
+
 
     return (
         <Container>
