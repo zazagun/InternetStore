1367f4e01f5be386ae103b257b2cb55c2942bc57
laptop <penis@gmail.com>
2026-01-13 14:52:45 +0400

    baskettem

---
+++ client/src/components/BasketItem.js
@@ -0,0 +1,13 @@
+import React, {useEffect} from "react"
+import  {addDevice, fetchFromBasket, deleteDevice } from "../http/deviceAPI"
+
+const BasketItem = () => {
+    fetchFromBasket()
+        .then()
+
+    return(
+        <p></p>
+    )
+}
+    
+export default BasketItem
